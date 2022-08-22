e87bcdce389353f45cc5e5ed739ffd36bbc0d13b
johnnywolinski <101757344+johnnywolinski@users.noreply.github.com>
2022-08-21 22:45:14 -0500

    finshed game for now - will add features at a later date
    going to add full words - more than 1 letter falling at once - styling

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 const grid = document.querySelector('.grid');
 let squares = Array.from(document.querySelectorAll('.grid div'));
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let misses = 0;
 let currentPos = 5;
-let k = 250;
+let k = 100;
 width = 9;
+score = 0;
 let randomLetter = letters[Math.floor(Math.random() * 26)]
 console.log(randomLetter);
 
 console.log(squares[currentPos])
-
 
 function draw(){
   squares[currentPos].classList.add('letter' + randomLetter);
@@ -17,45 +18,67 @@ function draw(){
 function undraw(){
   squares[currentPos].classList.remove('letter' + randomLetter);
 }
+function updateMisses(){
+  misses++;
+  document.getElementById('misses').innerHTML = "Misses: " + misses;
+}
+function updateScore(){
+  score++;
+  document.getElementById('score').innerHTML = "Score: " + score;
+}
 function moveDown(){
   undraw();
   console.log(currentPos)
   currentPos += 10;
-  if(currentPos > 195){
+  if(currentPos > 395){
+    updateMisses();
      randomLetter = letters[Math.floor(Math.random() * 26)];
      currentPos = Math.floor(Math.random() * 10);
   }
+  if(misses > 2){
+    clearInterval(timerId)
+    score = 0;
+      misses = 0;
+    startGame();
 
-draw();
+}else if(misses < 3){
+  draw();
+}
+
 }
 
 function newLetter(){
   randomLetter = letters[Math.floor(Math.random() * 26)]
   currentPos = Math.floor(Math.random() * 10);
    squares[currentPos].classList.add('letter' + randomLetter)
-     console.log(squares[currentPos]);
-     k -= 50;
-     console.log(k);
+
 }
 function changeLetter(){
   squares[currentPos].classList.remove('letter' + randomLetter);
-
-
+}
+function startGame(){
+  document.getElementById('misses').innerHTML = "Misses " + misses;
+document.getElementById('score').innerHTML = "Score: " + score;
+document.addEventListener('keydown', startGame2);
+}
+function startGame2(){
+document.removeEventListener('keydown', startGame2)
+  timerId = setInterval(moveDown, k)
 }
 
-timerId = setInterval(moveDown, k)
+
 function control(event){
-  if(event.key === randomLetter)
+  let randomLetterLC = randomLetter.toLowerCase();
+  if(event.key === randomLetterLC || event.key === randomLetter)
   {
     undraw();
+    updateScore();
     newLetter();
 }
-
+  else if(!event.key === randomLetterLC ){
+    misses++;
 }
+}
+startGame();
 document.addEventListener('keydown', control);
-
-
-
-
-
 })
